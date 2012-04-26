cc89d0a3a12e4fcf25dee974c405eaf43a3a1262
alepaez <alepaezseq@gmail.com>
2012-04-26 16:33:22 -0300

    Added init and ActAsStatus module

--- lib/act_as_status.rb
+++ lib/act_as_status.rb
@@ -1,5 +1,28 @@
-require "act_as_status/version"
+module ActsAsStatus
+  def acts_as_status( field, valid_values )
 
-module ActAsStatus
-  # Your code goes here...
+    status_variable = '@' + field.to_s.upcase() + '_STATUS'
+    instance_variable_set status_variable, valid_values
+
+    self.validates field, :inclusion => { :in => instance_variable_get(status_variable) }
+
+    valid_values.each do |valid_value|
+      define_method "#{valid_value}?" do
+        self.send("#{field}") == valid_value
+      end
+
+      (class << self; self; end).send(:define_method, "only_#{valid_value}") do
+        where(["#{field} = ?", valid_value])
+      end
+
+      (class << self; self; end).send(:define_method, "#{valid_value}") do
+        valid_value
+      end
+    end
+
+  end
+end
+
+if defined?(ActiveRecord::Base)
+  ActiveRecord::Base.extend ActsAsStatus
 end
